62f6eec8f000675011d088733335c92eec17f2c9
thomas-cloudmile <thomas.chiang@mile.cloud>
2024-07-22 09:12:38 +0800

    refactor: pydantic

--- api_gateway/main.py
+++ api_gateway/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
+from pydantic import BaseModel
 import grpc
 from file_service_pb2 import UploadRequest, RetrieveRequest
 from file_service_pb2_grpc import FileServiceStub
@@ -9,17 +10,30 @@ app = FastAPI()
 channel = grpc.insecure_channel('grpc_service:50051')
 stub = FileServiceStub(channel)
 
-@app.post("/api/files")
-def upload_file(record: dict):
-    request = UploadRequest(user_name=record['user_name'], file_name=record['file_name'], file_path=record['file_path'])
+class UploadFileRequest(BaseModel):
+    user_name: str
+    file_name: str
+    file_path: str
+
+class FileResponse(BaseModel):
+    name: str
+    path: str
+
+class UploadFileResponse(BaseModel):
+    status: str
+    message: str
+
+@app.post("/api/files", response_model=UploadFileResponse)
+def upload_file(record: UploadFileRequest):
+    request = UploadRequest(user_name=record.user_name, file_name=record.file_name, file_path=record.file_path)
     response = stub.UploadFile(request)
     if response.status == "success":
         return {"status": "success", "message": response.message}
     else:
         raise HTTPException(status_code=400, detail=response.message)
 
-@app.get("/api/files")
+@app.get("/api/files", response_model=list[FileResponse])
 def get_files(user_name: str):
     request = RetrieveRequest(user_name=user_name)
     response = stub.GetFiles(request)
-    return {"files": [{"name": file.file_name, "path": file.file_path} for file in response.files]}
+    return [{"name": file.file_name, "path": file.file_path} for file in response.files]
